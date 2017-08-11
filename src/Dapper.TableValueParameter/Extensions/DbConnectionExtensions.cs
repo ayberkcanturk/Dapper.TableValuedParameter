0209200b593bbe8f66801de74c7e5db38106929c
Ayberk Cantürk <ayberk.canturk@gmail.com>
2017-08-11 21:23:12 +0300

    IDbConnection extension method fixed

--- src/Dapper.TableValueParameter/Extensions/DbConnectionExtensions.cs
+++ src/Dapper.TableValueParameter/Extensions/DbConnectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 
 using Dapper.TableValuedParameter;
+using System.Collections.Generic;
 
 namespace Dapper.TableValueParameter.Extensions
 {
@@ -9,10 +10,10 @@ namespace Dapper.TableValueParameter.Extensions
     /// </summary>
     public static class DbConnectionExtensions
     {
-        public static void Query<TAny>(this IDbConnection connection, string sql, Tvp tvp, IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null) where TAny : class
+        public static IEnumerable<TAny> Query<TAny>(this IDbConnection connection, string sql, Tvp tvp, IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null) where TAny : class
         {
             //Tvp as object to prevent recursive loop
-            connection.Query<TAny>(sql, tvp as object, transaction, buffered, commandTimeout, commandType);
+            return connection.Query<TAny>(sql, tvp as object, transaction, buffered, commandTimeout, commandType);
         }
     }
 }
